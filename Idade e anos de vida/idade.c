09e2407c62ed5c82d05d05b6b90e2c14299adcd8
Marcos Pereira <100005524+MarcosMattap@users.noreply.github.com>
2026-03-06 10:41:09 +0000

    chore: organizar estrutura do projeto C e configurar VS Code
    - adiciona arquivos de configuração do VS Code (.vscode)
    - cria o programa `idade.c` em `Idade e anos de vida/`
    - move `olamundo.c` e `README.md` para `Olá Mundo/`

---
+++ Idade e anos de vida/idade.c
@@ -0,0 +1,16 @@
+#include <stio.h>
+
+int main(){
+   int Idade; //Reservando espaço para um inteiro
+   int Dias;
+   
+   printf("Professor: Quantos anos você tem? ");
+
+   //O '&' indica o endereço de memória onde a idade será guardada.
+   scanf("%d", &Idade);
+   Dias = Idade * 365;
+   
+   printf("Incrível! Você ja viveu pelomenos %d dias. \n" , Dias);
+
+   return 0;
+}
